9e5ee8e32fd452e2d1e5801af7535f7071fd966e
Dhyey Sapara <dhyey@simformsolutions.com>
2023-03-01 14:20:44 +0530

    refactor: TE7-T115 Introduction to Rails
    Converted static variables to instance variables and then accessed it.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
+    @College_name = "GEC-Gandhinagar"
     @your_name = "Dhyey Sapara"
+    @DOB = "15/12/2001"
+    @Hobbies = "Table Tennis, Gaming"
+    @Current_work = "Ruby on Rails(ROR)"
   end
 end
