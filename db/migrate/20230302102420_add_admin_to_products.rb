185135e4fcb1eb82ad53a9f25135bb3a231789f4
Dhyey Sapara <dhyey@simformsolutions.com>
2023-03-07 10:59:14 +0530

    refactor:TE7-T116 Active Record Basics
    Removed AddAdminToProducts migration file. Made changes in database.yml file.

--- db/migrate/20230302102420_add_admin_to_products.rb
+++
@@ -1,5 +0,0 @@
-class AddAdminToProducts < ActiveRecord::Migration[7.0]
-  def change
-    # Created this migration but not used it. I have downed the migration using 'rails db:migrate:down VERSION=20230302102420"
-  end
-end
