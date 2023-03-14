600b627de78e1793ddea5839aafebef285ef412f
Dhyey Sapara <dhyey@simformsolutions.com>
2023-03-14 17:25:14 +0530

    refactor: TE7-T117 Active Record Migrations
    Changed indentation and added new way to add class in our tags.

--- app/models/author.rb
+++ app/models/author.rb
@@ -5,5 +5,5 @@ class Author < ApplicationRecord
   validates :last_name, presence: true
   validates :email, presence: true, format: { with: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/, message: "Enter proper email address"}, uniqueness: true
 
-  # validates_date :date_of_birth, presence: true, on_or_before: lambda { Date.current }
+  validates_date :date_of_birth, presence: true, on_or_before: -> { Date.current }, message: "Enter proper date of birth"
 end
